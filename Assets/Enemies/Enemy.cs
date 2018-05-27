794cd76ec97b250c2a1b65dd9e178fa340433084
Braden Jageman <bjageman@surgeforward.com>
2018-05-27 10:29:02 -0700

    Fully Event Based Raycasting

---
+++ Assets/Enemies/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class Enemy : MonoBehaviour {
+
+	[SerializeField] int maxHealthPoints = 100;
+	[SerializeField] float currentHealthPoints = 100f;
+	[SerializeField] float attackRadius = 4f;
+
+	GameObject player = null;
+	ThirdPersonCharacter thirdPersonCharacter = null;
+	AICharacterControl aiCharacterControl;
+
+	public float healthAsPercentage{
+		get{
+			return currentHealthPoints / (float)maxHealthPoints;
+		}
+	}
+
+	void Start(){
+		player = GameObject.FindGameObjectWithTag("Player");
+		thirdPersonCharacter = GetComponent<ThirdPersonCharacter>();
+		aiCharacterControl = GetComponent<AICharacterControl>();
+	}
+
+	void Update(){
+		float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+		if (distanceToPlayer < attackRadius){
+			aiCharacterControl.SetTarget(player.transform);
+		}else{
+			aiCharacterControl.SetTarget(transform);
+		}
+	}
+}
